11732cdbc2a5e9b1c157cef734ab777f220b4b4e
Ryen <1278323157@qq.com>
2023-05-25 17:13:22 +0800

    规格参数下拉列表完成

--- assets/custom.js
+++ assets/custom.js
@@ -1,5 +1,10 @@
 $(function(){
 
+  $(document).on('click', '.accordion [tag-name="accordion-header"]', function(e) {
+    $(this).parent().find('[tag-name="accordion-content"]').slideToggle(500,"linear");
+    $(this).find('[tag-name="accordion-arrow"] svg:last-child').fadeToggle(500,"linear");
+  });
+
 
   if ( matchMedia('screen and (max-width: 768px)').matches ) {
     $('[section-name = store-features] .store-info').flickity( { prevNextButtons: true, pageDots: false } );
