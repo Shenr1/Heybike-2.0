c759f102583c8f5ba8a8458d8b989ace5a9aad06
Ryen <1278323157@qq.com>
2023-05-26 16:16:22 +0800

    移动端处理，bug处理，下拉增加图片与满屏文案

--- assets/custom.js
+++ assets/custom.js
@@ -5,9 +5,48 @@ $(function(){
     $(this).find('[tag-name="accordion-arrow"] svg:last-child').fadeToggle(500,"linear");
   });
 
-
+  // 移动端轮播处理
   if ( matchMedia('screen and (max-width: 768px)').matches ) {
-    $('[section-name = store-features] .store-info').flickity( { prevNextButtons: true, pageDots: false } );
+    $('[section-name = "store-features"] .store-info').flickity( { prevNextButtons: true, pageDots: false } );
+  }
+
+
+
+  // 产品页获取所有锚点链接
+  if($(".template-product").length){
+
+    const navLinks = $('[section-name = "product-tabs-nav"] [tag-name = "tabs-wrapper"] a');
+    // 产品页的锚点跳转
+    $('[section-name = "product-tabs-nav"] [tag-name = "tabs-wrapper"] a').on("click", function(event) {
+      event.preventDefault();
+      var target = $(this).attr("href");
+      var offset = $(target).offset().top + 1;
+      $("html, body").animate({
+          scrollTop: offset
+        }, 1500);
+    })
+
+    $(window).on("scroll", function() {
+      const scrollPosition = $(window).scrollTop();
+      for (var i = 0; i < navLinks.length; i++) {
+        const ele = $(navLinks.eq(i).attr('href'));
+        const nextEle = $(navLinks.eq(i+1).attr('href'));
+        const eleTop = ele.position().top;
+        const nextEleTop = $(navLinks.eq(i+1)).length?nextEle.position().top:scrollPosition+1;
+        if ( eleTop <= scrollPosition && nextEleTop > scrollPosition) {
+          navLinks.removeClass('active');
+          navLinks.eq(i).addClass('active');
+        }
+      }
+    });
+
+
+    // todo 需求不完整，规格怎么选，确定后掉api
+    $('[section-name = "product-tabs-nav"] [tag-name = "atc-btn"]').on("click", function(event) {
+        $(".product-action [type='submit']").click();
+    })
+    
+
 
   }
 
@@ -15,10 +54,13 @@ $(function(){
 
 
 
-
-
-
-
-
-
 })
+
+
+
+
+ 
+
+
+
+
