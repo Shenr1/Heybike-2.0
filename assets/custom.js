54a44b80a4cac14c2bf815d36b41c67046bdf6b9
Ryen <1278323157@qq.com>
2023-06-01 11:16:40 +0800

    多图文高度不一,footer logo长度.app多按钮

--- assets/custom.js
+++ assets/custom.js
@@ -5,32 +5,32 @@ $(function(){
     $(this).find('[tag-name="accordion-arrow"] svg:last-child').fadeToggle(500,"linear");
   });
 
-  // 轮播内嵌模式改写 todo 封装
-  const iniFlickity = (index) => {
-    console.log("iniFlickity");
-    const flickityEles = $("[hb-flickity]");
-    flickityEles.each(function(i) {
+  // 轮播内嵌模式改写 todo 封装 没用了
+  // const iniFlickity = (index) => {
+  //   console.log("iniFlickity");
+  //   const flickityEles = $("[hb-flickity]");
+  //   flickityEles.each(function(i) {
 
-      // 如果没传说明全部都初始化
-      // 如果传了就判断是否和当前的i相同,相同才初始化
-      if(index != null && index != i){ return }
-      const optionObj = $.parseJSON($(this).attr('hb-flickity'));
-      if ($(this).flickity) {
-        $(this).on( 'ready.flickity', (item) => { 
-          console.log("ready");
-          console.log($(this).height());
-          if($(this).height() < 100){ 
-            console.log("re"); 
-            iniFlickity(i) 
-          }
-        });
-        $(this).flickity(optionObj);
-      } else {
-        setTimeout(iniFlickity, 300);
-      }
-    });
-  }
-  iniFlickity()
+  //     // 如果没传说明全部都初始化
+  //     // 如果传了就判断是否和当前的i相同,相同才初始化
+  //     if(index != null && index != i){ return }
+  //     const optionObj = $.parseJSON($(this).attr('hb-flickity'));
+  //     if ($(this).flickity) {
+  //       $(this).on( 'ready.flickity', (item) => { 
+  //         console.log("ready");
+  //         console.log($(this).height());
+  //         if($(this).height() < 100){ 
+  //           console.log("re"); 
+  //           iniFlickity(i) 
+  //         }
+  //       });
+  //       $(this).flickity(optionObj);
+  //     } else {
+  //       setTimeout(iniFlickity, 300);
+  //     }
+  //   });
+  // }
+  // iniFlickity()
 
 
 
